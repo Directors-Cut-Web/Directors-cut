d8d09b5f8a432a26dec0d4c63cea542f19efea40
Mark Thompson <videopromptmaster@gmail.com>
2025-07-19 23:32:41 -0400

    fix: Correct module syntax in analyze-image API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { Route, Routes } from 'react-router-dom'
-import IndexPage from './pages/Index' // Or whatever you named your homepage component
-import StudioPage from './pages/StudioPage' // Or whatever you named your studio page component
+import IndexPage from './pages/Index' 
+import StudioPage from './pages/StudioPage' 
+// --- MODIFICATION: Import the new Runway form component ---
+import RunwayGen4Form from './components/RunwayGen4Form'; // Assuming the path is src/components/
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
       <Route path="/studio" element={<StudioPage />} />
-      {/* Add other routes for /about, /guides, etc. here */}
+      
+      {/* --- MODIFICATION: Added a temporary route for Runway ---
+          This is a placeholder to make the component visible.
+          We will integrate it into the StudioPage popup later.
+      */}
+      <Route 
+        path="/studio/runway" 
+        element={<RunwayGen4Form onPromptGenerated={(prompt) => console.log(prompt)} />} 
+      />
+
     </Routes>
   )
 }
